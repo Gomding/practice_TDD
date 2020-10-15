f7769600f6f6ff2a6e767dc755723eb625265b9b
Gomding <backchida@gmail.com>
2020-10-16 00:49:21 +0900

    [리팩토링]else 문을 없애고 null 또는 빈값 일 때 return 0 을 이용해서 반환

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -4,13 +4,14 @@ public class StringCalculator {
     public static int splitAndSum(String text) {
         int result = 0;
         if (text == null || text.isEmpty()) {
-            result = 0;
-        }else {
-            String[] values = text.split(",|:");
-            for (String value : values) {
-                result += Integer.parseInt(value);
-            }
+            return 0;
+        }
+        String[] values = text.split(",|:");
+        for (String value : values) {
+            result += Integer.parseInt(value);
         }
         return result;
     }
+
+
 }
