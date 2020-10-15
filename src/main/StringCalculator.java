5cc26465bae1fc03a1a3976c023ca78c56ca44d6
Gomding <backchida@gmail.com>
2020-10-16 00:51:55 +0900

    [리팩토링]배열로 만든 숫자들을 모두 더하는 메서드 생성

--- src/main/StringCalculator.java
+++ src/main/StringCalculator.java
@@ -2,16 +2,19 @@ package main;
 
 public class StringCalculator {
     public static int splitAndSum(String text) {
-        int result = 0;
         if (text == null || text.isEmpty()) {
             return 0;
         }
         String[] values = text.split(",|:");
+        return sum(values);
+    }
+
+    private static int sum(String[] values) {
+        int result = 0;
         for (String value : values) {
             result += Integer.parseInt(value);
         }
         return result;
     }
-
 
 }
